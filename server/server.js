d65ae1d5373e6bc208832fc9e4079362fe3b4ee0
Pratham shetty <prathamshetty484@gmail.com>
2024-06-22 17:29:38 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,38 @@
-import WebSocket from "ws";
+import express from 'express'
+const app = express();
+import http from 'http'
+import cors from 'cors'
+import { Server } from 'socket.io';
+app.use(cors());
 
-const server = new WebSocket.Server({ port: '3000' })
+const server = http.createServer(app);
 
-server.on('connection', socket => {
-    //listening for a message
-    socket.on('message', message => {
-        //const b = Buffer.from(message)
-        //console.log(b.toString())
-        socket.send(`${message}`)//message listened n sent back to whoever sent this
-    })
-})
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+//it is based on events few events are connection,join_room etc 
+//io.on means we listening for the inside named event and when it occurs that callback runs
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+    //all these are listened to after connection done
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
