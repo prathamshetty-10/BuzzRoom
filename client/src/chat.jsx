ad1a5b050ac4ff68f9d0c06fa7d4286fa6c7f6fd
Pratham shetty <prathamshetty484@gmail.com>
2024-06-23 00:55:15 +0530

    completed the mini project

--- client/src/chat.jsx
+++ client/src/chat.jsx
@@ -34,16 +34,16 @@ function Chat({socket,username,room}){
           setMessageList((list) => [...list, data]);
         });
       }, [socket]); //whenever there is change in socket the above function happens
-    return <div className="h-[70vh] w-[60vw] bg-gray-300 rounded-3xl p-[10px]">
-    <div className="h-[50px] font-bold text-2xl  border-b-2 border-black text-black mx-[100px] px-[300px]">
+    return <div className="h-[700px] w-[90%] lg:h-[70vh] lg:w-[60vw] bg-gray-300 rounded-3xl lg:p-[10px] mr-[20px] lg:mr-[0px]">
+    <div className="h-[50px] font-bold text-2xl  border-b-2 border-black text-black lg:mx-[100px] px-[130px] py-[10px] lg:py-[0px] sm:px-[280px] lg:px-[300px]">
         <p>Live chat</p>
     </div>
-    <div className=" h-[48vh] overflow-y-scroll">
-    <ScrollToBottom className="h-[48vh]">
+    <div className=" h-[550px] lg:h-[48vh] overflow-y-scroll">
+    <ScrollToBottom className="h-[550px] lg:h-[48vh]">
     {messageList.map((messageContent) => {
       return username === messageContent.author ?(
         <div
-          className=" w-[350px]  text-black my-[10px] mx-[50px]"
+          className=" w-[300px] lg:w-[350px]  text-black my-[10px] mx-[20px] lg:mx-[50px]"
           id={username === messageContent.author ? "you" : "other"}
         >
           <div>
@@ -58,11 +58,11 @@ function Chat({socket,username,room}){
         </div>
       ):(
         <div
-          className=" w-[350px]  text-black my-[10px] ml-[510px]"
+          className=" w-[300px] lg:w-[350px]  text-black my-[10px] ml-[60px] sm:ml-[350px] lg:ml-[510px]"
           id={username === messageContent.author ? "you" : "other"}
         >
           <div>
-            <div className="message-content bg-[#576ce6] min-h-[30px] rounded-2xl px-[15px] py-[5px] break-normal">
+            <div className="message-content bg-[#40b085] min-h-[30px] rounded-2xl px-[15px] py-[5px] break-normal">
               <p className=" w-full break-words">{messageContent.message}</p>
             </div>
             <div className="message-meta flex gap-4 px-[20px] ">
@@ -78,8 +78,8 @@ function Chat({socket,username,room}){
     
     </div>
     <div className="h-[40px]"></div>
-        <input type="text" value={currMessage} placeholder="send a message" className="h-[50px] w-[40vw] ml-[70px] bg-transparent border-black text-black rounded-3xl border-2 px-[40px]" onChange={handle}/>
-        <button onClick={sendMessage} className="w-[10vw] h-[50px] bg-yellow-500 text-black font-bold text-xl mx-[30px] rounded-3xl border-2 border-black hover:bg-yellow-300">Send</button>
+        <input type="text" value={currMessage} placeholder="send a message" className="h-[50px] w-[60vw] lg:w-[40vw] ml-[20px] lg:ml-[70px] bg-transparent border-black text-black rounded-3xl border-2 px-[40px]" onChange={handle}/>
+        <button onClick={sendMessage} className="w-[20vw] lg:w-[10vw] h-[50px] bg-yellow-500 text-black font-bold text-xl mx-[10px] md:mx-[30px] rounded-3xl border-2 border-black hover:bg-yellow-300">Send</button>
     
     
     </div>
